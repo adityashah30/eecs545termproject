9cc0b7f4cf13c9c730f8ec1c447494ae90b8db1b
Akhila Rallapalli <AkhilaRallapalli@Akhilas-MacBook-Pro.local>
2016-04-09 18:14:14 -0400

    Methods in organism implemented

--- code/organism.py
+++ code/organism.py
@@ -1,4 +1,4 @@
-
+Iimport numpy as np
 
 class Organism:
     '''
@@ -8,8 +8,9 @@ class Organism:
                  nodes in the hidden layer.
     '''
 
-    def __init__(self, feature_subset=[], hidden_nodes=None):
+    def __init__(self, count,feature_subset=[], hidden_nodes=None):
         self.feature_subset = feature_subset
+        self.count = count #num of features
         if hidden_nodes:
             self.hidden_nodes = hidden_nodes
             self.use_neural_net = True
@@ -17,14 +18,38 @@ class Organism:
             self.use_neural_net = False
 
     def mutate(self):
+        subsetsize = len(feature_subset)
+        index = np.random.randint(0,subsetsize)
+        feature = np.random.randint(0,self.count)
+        if feature in self.feature_subset:
+            feature = np.random.randint(0,self.count)
+        else
+            self.feature_subset[index] = feature
+
         pass
 
     def reproduce(self, other):
-        pass
+        subsetsize1 = len(self.feature_subset)
+        subsetsize2 = len(other.feature_subset)
+        size = np.maximum(subsetsize1,subsetsize2)
+        crossoverpoint = np.random.randint(1,size-1)
+        childfeatures = []
+        childfeatures = self.feature_subset[:crossoverpoint] + other.feature_subset[crossoverpoint:]
+        childcount = len(childfeatures)
+        child = Organism(childcount,childfeatures)
+        return child
+        #pass
 
     def fitness_measure(self):
         pass
 
     @staticmethod
-    def init_random():
+    def init_random(subsetsize):
+        count = 40 #num of features
+        features = np.random.randint(0,count,size = subsetsize)
+        Org = Organism(count,features)
+
+
+
+
         pass
