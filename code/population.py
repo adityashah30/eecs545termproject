299082c5a38d91f79493e47ce76d282b8fb7fbeb
sharath <nssharath93@gmail.com>
2016-04-11 19:58:20 -0400

    Population code added

--- code/population.py
+++ code/population.py
@@ -16,4 +16,41 @@ class Population:
         self.population = [Organism.init_random() for _ in xrange(size)]
 
     def create_next_gen(self):
-        pass
+	 for organism in population:
+       fitness_val = fitness(individual)
+
+      # Generate the (individual,fitness) pair, taking in account whether or
+      # not we will accidently divide by zero.
+      if fitness_val == 0:
+        pair = (individual, 1.0)
+      else:
+        pair = (individual, 1.0/fitness_val)
+
+      weighted_population.append(pair)
+
+    population = []
+
+    # Select two random individuals, based on their fitness probabilites, cross
+    # their genes over at a random point, mutate them, and add them back to the
+    # population for the next iteration.
+    for _ in xrange(POP_SIZE/2):
+      # Selection
+      ind1 = weighted_choice(weighted_population)
+      ind2 = weighted_choice(weighted_population)
+
+      # Crossover
+      ind1, ind2 = crossover(ind1, ind2)
+
+      # Mutate and add back into the population.
+      population.append(mutate(ind1))
+      population.append(mutate(ind2))
+
+  # Display the highest-ranked string after all generations have been iterated
+  # over. This will be the closest string to the OPTIMAL string, meaning it
+  # will have the smallest fitness value. Finally, exit the program.
+  fittest_string = population[0]
+  minimum_fitness = fitness(population[0])
+		
+	   
+	   
+		
