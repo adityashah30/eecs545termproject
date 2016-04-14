33978f57feb2669bb341bf1231b7e395f7262d82
Akhila Rallapalli <AkhilaRallapalli@Akhilas-MacBook-Pro.local>
2016-04-13 22:14:05 -0400

    changes in population

--- code/population.py
+++ code/population.py
@@ -1,12 +1,16 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 12 21:27:18 2016
-
 @author: SHARATH NS
 """
 
 from organism import Organism
 import random
+from operator import itemgetter
+import math
+import numpy as np
+
 
 class Population:
     '''
@@ -14,7 +18,6 @@ class Population:
     1. The size of the population
     2. The elitism ratio
     3. The mutation ratio
-
     '''
 
     def __init__(self, size=100, elitism=0.15, mutation=0.3):
@@ -46,13 +49,38 @@ class Population:
             weighted_population.append(pair)
         
         new_population = [];
-        for _ in range(self.pop_size):
+        sorted(weighted_population,key=itemgetter(1));
+        leng=len(weighted_population);
+        eli=(math.floor((self.elitism)*self.pop_size))
+        new_weightedpop=np.asarray(weighted_population);
+        
+        
+#        for i in range(math.floor((self.elitism)*leng)):
+#            new_population.append(weighted_population)
+        new_population=new_weightedpop[0:eli,0].tolist();
+        children=[];
+        print (self.pop_size)
+        for _ in range(int(self.pop_size)):
             ind1 = self.weighted_choice(weighted_population)
             ind2 = self.weighted_choice(weighted_population)
             child = ind1.reproduce(ind2)
-            new_population.append(child.mutate)
+            
+            
+            if (random.uniform(0,1)<=self.mutation):
+                child.mutate();
+            fitness_val = child.fitness_measure()
+            children.append((child,fitness_val))
+            
+            
+        sorted(children,key=itemgetter(1),reverse=True);
+        leng=len(children);
+        eli_new=(math.floor((1-self.elitism)*self.pop_size))
+        new_children=np.asarray(children)
+        final_children=new_children[0:eli_new,0].tolist();
+            
+        new_population=new_population+final_children
         
-        new_gen.pop_size=self.pop_size
+        new_gen.pop_size=len(new_population)
         new_gen.elitism=self.elitism
         new_gen.mutation=self.mutation
         new_gen.new_population=new_population
@@ -66,5 +94,4 @@ class Population:
                 
                 
             
-        
-   
+    
