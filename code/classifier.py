e77c876efc5c462bf73190647ef9120a48fe6bd0
Akhila Rallapalli <AkhilaRallapalli@Akhilas-MacBook-Pro.local>
2016-04-17 00:33:57 -0400

    added lda,nb,rf

--- code/classifier.py
+++ code/classifier.py
@@ -3,12 +3,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.cross_validation import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 from keras.models import Sequential
 from keras.utils import np_utils
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import RMSprop, SGD
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.lda import LDA
 
 class NNKeras:
     '''
@@ -134,20 +138,25 @@ class SVMClassifier:
 
     def fit_score(self, data, feature_set):
         X, Y = data['X'][:,feature_set], data['Y']
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=self.seed)
+        #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=self.seed)
 
         scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
+        X_train = scaler.fit_transform(X)
+        #X_test = scaler.transform(X_test)
 
         nb_features = X_train.shape[1]
 
         model = svm.SVC()
+        test_score = cross_val_score(model, X, Y, scoring='roc_auc', cv=10, verbose=0)
+        test_score = np.mean(test_score)
+        '''
+        test_score = np.mean(test_score)
         model.fit(X_train, Y_train)
 
         Y_pred = model.predict(X_test)
         test_score = roc_auc_score(Y_test, Y_pred)
         # print test_score
+        '''
         return test_score
 
 class LogisitcRegClassifier:
@@ -163,11 +172,11 @@ class LogisitcRegClassifier:
     def fit_score(self, data, feature_set):
         # X_train, Y_train = data['X'][:,feature_set], data['Y']
         X, Y = data['X'][:,feature_set], data['Y']
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=self.seed)
+        #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=self.seed)
 
         scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
+        X_train = scaler.fit_transform(X)
+        #X_test = scaler.transform(X_test)
 
         nb_features = X_train.shape[1]
 
@@ -185,9 +194,110 @@ class LogisitcRegClassifier:
         print test_score
         return test_score
 
+class GaussianNBClassifier:
+    '''
+    The NaiveBayes class. Takes in a genotype and trains a classifier model
+    on a subset of the training set and returns evaluation score on test set
+    which is again derived from the training set.
+    '''
+
+    def __init__(self):
+        self.seed = 42
+
+    def fit_score(self, data, feature_set):
+        # X_train, Y_train = data['X'][:,feature_set], data['Y']
+        X, Y = data['X'][:,feature_set], data['Y']
+        # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=self.seed)
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+        # X_test = scaler.transform(X_test)
+
+        model = GaussianNB()
+
+        test_score = cross_val_score(model, X, Y, scoring='roc_auc', cv=10, verbose=0)
+        test_score = np.mean(test_score)
+        # model.fit(X_train, Y_train)
+        # print test_score
+
+        # Y_pred = model.predict(X_test)
+        # test_score = roc_auc_score(Y_test, Y_pred)
+
+        # test_score = model.score(X_test, Y_test)
+        #print (test_score)
+        return test_score
+
+
+class RFClassifier:
+    '''
+    The RnadomForest Classifier class. Takes in a genotype and trains a classifier model
+    on a subset of the training set and returns evaluation score on test set
+    which is again derived from the training set.
+    '''
+
+    def __init__(self):
+        self.seed = 42
+
+    def fit_score(self, data, feature_set):
+        # X_train, Y_train = data['X'][:,feature_set], data['Y']
+        X, Y = data['X'][:,feature_set], data['Y']
+        # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=self.seed)
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+        # X_test = scaler.transform(X_test)
+
+        model = RandomForestClassifier()
+
+        test_score = cross_val_score(model, X, Y, scoring='roc_auc', cv=10, verbose=0, n_jobs=-1)
+        test_score = np.mean(test_score)
+        # model.fit(X_train, Y_train)
+        # print test_score
+
+        # Y_pred = model.predict(X_test)
+        # test_score = roc_auc_score(Y_test, Y_pred)
+
+        # test_score = model.score(X_test, Y_test)
+        #print (test_score)
+        return test_score
+
+class LDAClassifier:
+    '''
+    The NaiveBayes class. Takes in a genotype and trains a classifier model
+    on a subset of the training set and returns evaluation score on test set
+    which is again derived from the training set.
+    '''
+
+    def __init__(self):
+        self.seed = 42
+
+    def fit_score(self, data, feature_set):
+        # X_train, Y_train = data['X'][:,feature_set], data['Y']
+        X, Y = data['X'][:,feature_set], data['Y']
+        # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=self.seed)
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+        # X_test = scaler.transform(X_test)
+
+        model = LDA()
+
+        test_score = cross_val_score(model, X, Y, scoring='roc_auc', cv=10, verbose=0, n_jobs=-1)
+        test_score = np.mean(test_score)
+        # model.fit(X_train, Y_train)
+        # print test_score
+
+        # Y_pred = model.predict(X_test)
+        # test_score = roc_auc_score(Y_test, Y_pred)
+
+        # test_score = model.score(X_test, Y_test)
+        #print (test_score)
+        return test_score
+
 
 def classifier_factory(solve_method="keras", hidden_nodes=10):
     solve_method = solve_method.lower()
+    #print(solve_method)
     if solve_method == "keras":
         return NNKeras(hidden_nodes)
     elif solve_method == "pybrain":
@@ -196,3 +306,10 @@ def classifier_factory(solve_method="keras", hidden_nodes=10):
         return SVMClassifier()
     elif solve_method == "logistic":
         return LogisitcRegClassifier()
+    elif solve_method == "gaussiannb":
+        return GaussianNBClassifier()
+    elif solve_method == "randomforest":
+        return RFClassifier()
+    elif solve_method == "lda":
+        return LDAClassifier()
+    
